90337413168945d411a6d6e2b1ba33890e426e8f
Joshua Shelto <joshuashelto@gmail.com>
2018-11-18 16:58:37 -0500

    Update url redirect

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -3,12 +3,23 @@ class UrlController < ApplicationController
     #displays the top 100 most visited urls
     def top
       @top_urls = Url.most_visited
+      respond_to do |format|
+        format.html
+        format.json  { render :json => @top_urls }
+      end
     end
 
     def redirect_shortened_url
       friendly_url = params["friendly_url"]
-      original_url = Url.find_by(friendly_url: friendly_url).original_url
-      return redirect_to original_url
+      url = Url.find_by(friendly_url: friendly_url)
+      if url.present?  
+        original_url = url.original_url
+        url.update(visits: (url.visits + 1)) #increments the number of visits by 1 
+        return redirect_to original_url
+      else 
+        #add error here
+      end
     end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get "/top", to: "url#top", format: true
+
   get "/:friendly_url", to: "url#redirect_shortened_url"
+
 end
