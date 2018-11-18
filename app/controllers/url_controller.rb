8f36bbda712ea128523599f26840a54d8756e557
Joshua Shelto <joshuashelto@gmail.com>
2018-11-18 16:32:03 -0500

    Add redirect action for friendly_urls

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -5,4 +5,10 @@ class UrlController < ApplicationController
       @top_urls = Url.most_visited
     end
 
+    def redirect_shortened_url
+      friendly_url = params["friendly_url"]
+      original_url = Url.find_by(friendly_url: friendly_url).original_url
+      return redirect_to original_url
+    end
+
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -3,17 +3,22 @@ class Url < ApplicationRecord
 	validates :original_url, :url => true #via valid_url gem
 	validates_uniqueness_of :original_url #prevents duplicate records
 
-	after_create :generate_friendly_url #record needs id to create friendly_url
+	after_create :save_friendly_url #record needs id to create friendly_url
 
-	def generate_friendly_url
+	def save_friendly_url 
 		if self.valid? #includes url validation 
 			# self.save
-			url = original_url
 			
 			#friendly url is generated using id
 			#how to factor in same domain, paths, and params to minimize number of records
-
+			self.friendly_url = generate_friendly_url(self.id)
+			self.save!
 		end
+	end
+
+	def generate_friendly_url(id)
+		id = self.id
+
 	end
 
 	#Returns the urls, count order descending. Defaults to 100 records
