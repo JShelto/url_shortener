e959d0bac757a9585e0234d901d0ef559d2fad69
Joshua Shelto <joshuashelto@gmail.com>
2018-11-18 21:51:20 -0500

    Add methods to encode and decode ids

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,8 @@
 class Url < ApplicationRecord
 	require 'open-uri' #needed for Nokogiri
 
+	CHAR_MAP = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 	validates :original_url, :url => true #via valid_url gem
 	validates_uniqueness_of :original_url #prevents duplicate records
 
@@ -19,8 +21,15 @@ class Url < ApplicationRecord
 
 	def generate_friendly_url(id)
 		id = self.id
-		"applepit"
+		Url.convert_to_base(id)
 	end
+
+	#returns the original url based on the friendly url
+	def self.return_original_url(str)
+		id = Url.convert_from_base(str)
+		url = Url.find(id).original_url
+	end
+
 
 	#Returns the urls, count order descending. Defaults to 100 records
 	def self.most_visited(count: 100)
@@ -33,11 +42,6 @@ class Url < ApplicationRecord
 		self.save
 	end
 
-	#returns the original url based on the friendly url
-	def self.return_original_url
-
-	end
-
 	def self.get_title(friendly_url)
 		url = Url.find_by(friendly_url: friendly_url)
 		doc = Nokogiri::HTML(open(url.original_url))
@@ -47,5 +51,28 @@ class Url < ApplicationRecord
 			url.save!
 		end
 	end
+
+	private
+		def self.convert_to_base(num, base = 62) #convert
+			return CHAR_MAP[0] if num == 0
+			result = ""
+			while num > 0
+				r = num % base
+				result.prepend(CHAR_MAP[r])
+				num = (num / base).floor
+			end
+			result
+		end
+
+		def self.convert_from_base(value, base = 62)
+			base_10_num = 0
+			digit = 0
+			value.to_s.reverse.each_char do |char|
+				num = CHAR_MAP.index(char)
+				base_10_num += num * (base ** digit)
+				digit += 1
+			end
+			base_10_num
+		end
         
 end
