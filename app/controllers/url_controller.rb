4035cfb72a620587336ad1ccd2e4b8073e0a87a5
Joshua Shelto <joshuashelto@gmail.com>
2018-11-19 23:49:41 -0500

    Update show method

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -10,6 +10,7 @@ class UrlController < ApplicationController
     end
   end
 
+  #creates Url object using original url and returns friendly url
   def create
     u = Url.find_or_create_by(original_url: params["url"])
 
@@ -19,19 +20,18 @@ class UrlController < ApplicationController
     end
   end
 
+
+  #redirects friendly url to original url or returns an error
   def show
     friendly_url = params["friendly_url"]
-    url = Url.find_by(friendly_url: friendly_url)
-    if url.present?  
-      original_url = url.original_url
-      url.track_visit #increments the number of visits by 1 
+    u = Url.find_by(friendly_url: friendly_url)
+    if u.present?  
+      original_url = u.original_url
+      u.track_visit #increments the number of visits by 1 
 
       return redirect_to original_url, status: 301
     else 
-      respond_to do |format|
-        format.html
-        format.json { render :json => "No url found for #{full_path(request) + u.friendly_url}"}
-      end
+      render :json => {:error => "No url found for #{full_path(request) + friendly_url}"}.to_json, :status => 404
     end
   end
 
